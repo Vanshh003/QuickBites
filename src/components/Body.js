8c6bda82d7004adf035fee3126fbde64ddec70a5
Vanshh003 <vanshaggarwal003@gmail.com>
2024-08-28 20:08:10 +0530

    Dynamic/Live Data fetching and Live API Calling (swiggy)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,45 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 
 
 const Body = () => {
     // local state variable
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurants] = useState(); 
     
-    // as use state returns an array.. the above line can also be written as
-    // const arr = useState(resList);
-    // const [listOfRestaurants, setListOfRestaurants] = arr;
+
     
-    // it can also be written as
-    // const arr = useState(resList);
-    // const listOfRestaurants = arr[0];
-    // const setListOfRestaurants = arr[1];
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.40487574339156&lng=77.33804125338793&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+        const json = await data.json();     // convert that stream to json
+
+        // updating the state variable, for it to populate the app with new data
+        setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    };
     
     
 
-
-    
-    
-    // not using keys (not acceptable) <<<< index as key <<<<< unique id (best practice)
+    // not using keys (not acceptable) <<<< index as key <<<< unique id (best practice)
     return (
         <div className='body'>
             <div className='filter'>
+                
                 <button className="filter-btn" onClick={() => {
                     // filter logic here
                     const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
                     
                     // update state variable
-                    setListOfRestaurants(filteredList);
+                    setListOfRestaurants(filteredList);  
                 }}>
                     Top Rated Restaurants
                 </button>
+
             </div>
+
             <div className='res-container'>
                 {listOfRestaurants.map((restautant) => (   // Content Driven UI
                     <RestaurantCard key={restautant.info.id} resData={restautant}/>
                 ))}
             </div>
+            
         </div>
     )
 };
